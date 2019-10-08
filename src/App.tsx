8edc1d5825a15e07f4f824421cccea62934ba1e8
Aleksi Vankka <alsu.96@hotmail.fi>
2019-10-08 21:42:28 +0300

    Add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import Computers from './components/Computers'
 import './App.css'
 import logo from './assets/Bunkkeri-gg-200p.png'
 
 const App: React.FC = () => {
+  ReactGA.initialize('UA-129871257-2')
+  ReactGA.pageview(window.location.pathname + window.location.search)
+
   return (
     <div className="container">
       <div
